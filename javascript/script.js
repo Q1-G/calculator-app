e5f33c7c866f906d3a572937bc124d7805ee65d6
Q <156905888+Q1-G@users.noreply.github.com>
2025-10-31 17:34:27 +0200

    buttons have been programmed

--- javascript/script.js
+++ javascript/script.js
@@ -1,29 +1,142 @@
-const casing = document.getElementsByClassName('casing');
+const alignmentContainer = document.querySelector('.alignmentContainer')
+const screen = document.querySelector('.screen');
+const backspace = document.querySelector('.backspace');
+const power = document.querySelector('.power');
+const percent = document.querySelector('.percent');
+const divide = document.querySelector('.divide');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const multiply =  document.querySelector('.multiply');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const subtract = document.querySelector('.subtract');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const add = document.querySelector('.add');
+const change = document.querySelector('.change');
+const zero = document.querySelector('.zero');
+const point = document.querySelector('.point');
+const equals = document.querySelector('.equals');
+const buttons = document.querySelector('button');
 
-const buttons = document.getElementsByClassName('buttons');
+// ***************eventlisteners***************
 
+zero.addEventListener('click', () => {
+    const zeroValue = zero.value;
+    screen.textContent += zeroValue;
+});
 
+one.addEventListener('click', () => {
+    const oneValue = one.value;
+    screen.textContent += oneValue;
+});
 
-let num1 = 0;
-let num2 = 0;
-let opperators = ["/","*","+","-"];
+two.addEventListener('click', () => {
+    const twoValue = two.value;
+    screen.textContent += twoValue;
+});
 
-function add(num1,num2){
-    return num1 + num2
-}
+three.addEventListener('click', () => {
+    const threeValue = three.value;
+    screen.textContent += threeValue;
+});
 
-function subtract(num1,num2){
-    return num1 - num2
-}
+four.addEventListener('click', () => {
+    const fourValue = four.value;
+    screen.textContent += fourValue;
+});
 
-function multiply(num1,num2){
-    return num1 * num2   
-}
+five.addEventListener('click', () => {
+    const fiveValue = five.value;
+    screen.textContent += fiveValue;
+});
 
-function divide(num1,num2){
-    return num1 / num2
-}
+six.addEventListener('click', () => {
+    const sixValue = six.value;
+    screen.textContent += sixValue;
+});
 
-function display(){}
+seven.addEventListener('click', () => {
+    const sevenValue = seven.value;
+    screen.textContent += sevenValue;
+});
 
-buttons.addEventListner("click",display);
+eight.addEventListener('click', () => {
+    const eightValue = eight.value;
+    screen.textContent += eightValue;
+});
+
+nine.addEventListener('click', () => {
+    const nineValue = nine.value;
+    screen.textContent += nineValue;
+});
+
+backspace.addEventListener('click', () => {
+    let currentValue = screen.textContent;
+    if (currentValue.length > 0) { 
+    screen.textContent = currentValue.slice(0, -1)
+    }
+});
+
+power.addEventListener('click', () => {
+    screen.textContent = '';
+});
+
+percent.addEventListener('click',() => {
+    let currentValue = screen.textContent;
+    if (currentValue) { 
+        let answer = parseFloat(currentValue) / 100;
+        screen.textContent = answer;
+    }
+});
+
+divide.addEventListener('click', () => {
+    const division = divide.value;
+    screen.textContent += division;
+});
+
+multiply.addEventListener('click',() => {
+    const multiplication = multiply.value;
+    screen.textContent += multiplication;
+});
+
+subtract.addEventListener('click',() => {
+    const subtraction = subtract.value;
+    screen.textContent += subtraction;
+});
+
+add.addEventListener('click',() => {
+    const addition = add.value;
+    screen.textContent += addition;
+});
+
+equals.addEventListener('click', () => {
+    let expression = screen.textContent;
+    let mathExpression = expression.replace(/÷/g, '/').replace(/x/g, '*');
+
+    try {
+        let result = eval(mathExpression);
+
+        screen.textContent = result;
+    } catch (error) {
+        screen.textContent = 'Error';
+    }
+});
+
+point.addEventListener('click',() => {
+    const dot = point.value;
+    screen.textContent += dot;
+});
+
+change.addEventListener('click',() => {
+    let currentValue = screen.textContent;
+    if (currentValue >= 0 || currentValue < 0) { 
+        screen.textContent = -currentValue;
+    } else if (currentValue < 0) {
+        let positiveNumber = Math.abs(currentValue);
+        screen.textContent = positiveNumber;
+    }
+});
